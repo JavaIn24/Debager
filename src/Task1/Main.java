0618b6d69aa6e07cbc1e69e9b39cd02323ed720c
ярослав <yaracrut245@gmail.com>
2024-08-13 14:13:00 +0500

    Попытка 1

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -1,2 +1,15 @@
-package Task1;public class Main {
+package Task1;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer [] arrayNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 11};
+        Optional <Integer> max = Arrays.stream(arrayNumbers).filter(num -> num % 2 == 0 ).max(Integer::compare);
+        System.out.println(max.get());
+
+    }
+
 }
