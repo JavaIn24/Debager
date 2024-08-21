1629feaae177e172e43823af85869f0e0d373917
ярослав <yaracrut245@gmail.com>
2024-08-21 13:54:46 +0500

    Попытка 1

--- src/Task2/Main.java
+++ src/Task2/Main.java
@@ -1,2 +1,20 @@
-package Task2;public class Main {
+package Task2;
+
+public class Main {
+    public static void main(String[] args) {
+        String happyNewYear = "Привет, Александр, с праздником, с Новым Годом";
+
+        char[] happyNewYearCharArray = happyNewYear.toCharArray();
+
+        for (int i = 0; i < happyNewYearCharArray.length; i++) {
+            char a = happyNewYearCharArray[i];
+            if (Character.isUpperCase(a)) {
+                happyNewYearCharArray[i] = Character.toLowerCase(a);
+            } else if (Character.isLowerCase(a)) {
+                happyNewYearCharArray[i] = Character.toUpperCase(a);
+            }
+        }
+
+        System.out.println(new String(happyNewYearCharArray));
+    }
 }
